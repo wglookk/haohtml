48880e2b635f5c8f45f56309bde5be77a720b4ad
wugl <wugl@skong.com>
2017-04-10 15:54:10 +0800

    多项自定义滚动条
    Signed-off-by: wugl <wugl@skong.com>

---
+++ float0815/js/autoRoll2.js
@@ -0,0 +1,154 @@
+function autoRoll(id,hh){
+
+var obj = $('#'+ id)
+//结构格式化
+var obj_heights;
+if(arguments.length>1){
+	obj_heights = hh;
+	}else{
+	obj_heights = obj.height();
+	}
+console.log(obj_heights)
+var objcontent  = '';
+    objcontent += '<div class="rollbox" style="height:' + obj_heights + 'px;' + 'width:' + obj.width() + 'px;">';
+    objcontent +=     '<div class="rollcon" style="height:' + obj_heights + 'px;' + 'width:' + parseInt(obj.width()+24) + 'px;">';
+    objcontent +=     '<div class="rollconadd" style="width:' + parseInt(obj.width()-20) + 'px;">';
+    objcontent +=      obj.html();
+    objcontent +=     '</div>';
+    objcontent +=     '</div>';
+    objcontent +=     '<div class="rollsign" style="height:' + obj_heights + 'px;">';
+    objcontent +=     '<div class="rollsignobj"></div>';
+    objcontent +=     '</div>';    
+    objcontent += '</div>';
+obj.html(objcontent)
+//
+
+myboxroll(obj.find(".rollcon"),obj.find(".rollconadd"))
+signroll(obj.find(".rollcon"),obj.find(".rollconadd"))
+mouseroll(obj.find(".rollcon"),obj.find(".rollconadd"),document.getElementById(id))
+}
+//==============================================
+function myboxroll(winbox,wincon){
+	var sign_top = winbox.scrollTop()*(winbox.height()/wincon.height())
+	winbox.parent().find(".rollsignobj").css("top",sign_top +'px')
+	winbox.scroll(function(){
+		sign_top = winbox.scrollTop()*(winbox.height()/wincon.height())
+		//滚标赋top
+		winbox.parent().find(".rollsignobj").css("top",sign_top +'px')
+	
+	})
+}
+
+///////////////////////////////////////////////
+//sign滚标函数
+function signroll(winbox,wincon){
+//
+	
+	if(wincon.height()<=winbox.height()){
+		winbox.parent().find(".rollsign").hide()		
+	}else{
+		winbox.parent().find(".rollsign").show()		
+	}
+	var sign_h = parseInt(winbox.height()/wincon.height() * winbox.height());
+	
+	var win_h = winbox.height();
+	var rolly
+
+	//var sign_h = $(".myboxcon").height()/$(".mycon").height() * $(".myboxcon").height()
+	winbox.parent().find(".rollsign").height(win_h)
+	winbox.parent().find(".rollsignobj").height(sign_h)
+
+//
+	winbox.parent().find(".rollsign").mousedown(function(ev){
+		
+		var parenty = ev.pageY;
+		
+		var objtop = winbox.scrollTop()*(winbox.height()/wincon.height())
+		
+		
+		//
+		$(document).mousemove(function(e){
+
+			rolly = e.pageY - parenty + objtop
+			if( rolly > 0 && rolly < (win_h-sign_h)){
+				winbox.parent().find(".rollsignobj").css("top",rolly)
+				winbox.parent().find(".rollcon").scrollTop(rolly*(wincon.height()/winbox.height()))			
+			}
+			if( rolly < 0){
+				winbox.parent().find(".rollsignobj").css("top",0)
+				winbox.parent().find(".rollcon").scrollTop(0)
+			}
+			if(rolly > (win_h-sign_h)){
+				winbox.parent().find(".rollsignobj").css("top",win_h-sign_h)
+				winbox.parent().find(".rollcon").scrollTop((win_h-sign_h)*(wincon.height()/winbox.height()))			
+			}
+			
+		})
+		//
+		
+	
+	})
+//
+	$("html").mouseup(function(){
+	
+		$(document).unbind();
+		
+		
+	})
+//如果鼠标滑出窗口停止事件
+//	$("html").mouseout(function(){
+//	
+//		$(document).unbind();
+//		
+//		
+//	})
+//
+}
+///////////////////////////////////////////////
+var sign_topwheel
+function myboxrollwheel(winbox,wincon,num){
+	 winbox.scrollTop(winbox.scrollTop()+num)
+	 sign_topwheel = winbox.scrollTop()*(winbox.height()/wincon.height())
+
+		//滚标赋top
+		winbox.parent().find(".rollsignobj").css("top",sign_topwheel +'px')
+	
+	
+}
+//滚轮事件
+
+function mouseroll(winbox,wincon,objroll){
+//mousewheel事件区别下firefox
+if(window.navigator.userAgent.indexOf("Firefox") != -1){
+	objroll.addEventListener("DOMMouseScroll", function(event) {
+	    //console.dir(event);	
+		//console.log(event.detail)
+	//下滚值3
+			if(event.detail == 3){
+			myboxrollwheel(winbox,wincon,20)
+			}
+	//上滚值-3
+			if(event.detail == -3){
+			myboxrollwheel(winbox,wincon,-20)
+			}
+	});	
+	
+}else{
+	objroll.onmousewheel = function(event){
+		event = event || window.event;
+		//console.dir(event);	
+		//console.log(event.wheelDelta)
+	//下滚值-120
+			if(event.wheelDelta == -120){
+			myboxrollwheel(winbox,wincon,20)
+			}
+	//上滚值120
+			if(event.wheelDelta == 120){
+			myboxrollwheel(winbox,wincon,-20)
+			}
+		
+	};	
+	
+}
+
+}
